c604f4f071bf4ca6d7da9a1b347bc9f6bb4a4f64
Sneha-heha <kpsneha19@gmail.com>
2026-09-07 12:29:51 +0530

    Create file10.c

---
+++ file10.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+int data;
+struct node *prev;
+struct node *next;
+};
+void insert(struct node **head,int x,int pos){
+struct node *newnode = malloc(sizeof(struct node));
+newnode->data = x;
+}
+if (pos == 1){
+newnode ->prev = NULL;
+newnode ->next =*head;
+if(*head !=NULL){
+(*head)->prev = newnode;
+*head = newnode;}
+}
+else {
+struct node *temp = *head;
+for(int i = i;i< pos -1 && temp != NULL;i++)
+temp = temp->next;
+if(temp !=NULL){
+newnode->next = temp->next;
+newnode->prev = temp;
+if(temp->nexr!=NULL)
+temp->next->prev = newnode;
+temp->next = newnode;
+else{
+free(newnode);}
+}
+
+}
+void display(){
+}
+int main(){
+struct node*head = NULL;
+insert(&head,10,1);
+insert(&head,20,1);
+insert(&head,30,1);
+display(head);
+printf("code completed\n");
+
+return 0;
+}
